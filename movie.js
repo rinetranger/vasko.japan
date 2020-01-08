c035620f61247bae5ae9a8545724136d12dc9a40
Rin Kishi <rinetranger@gmail.com>
2020-01-08 15:39:10 +0900

    main5&css

--- movie.js
+++ movie.js
@@ -40,14 +40,3 @@ function onPlayerStateChange(e) {
     }
 }
 
-
-$(function(){
-    $('a[href^=/#home]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-    });
-    });
